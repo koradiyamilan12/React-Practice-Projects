128342340d37e893c3397da8abc053441d851e51
koradiyamilan12 <milankoradiya.work@gmail.com>
2025-11-19 18:23:15 +0530

    feat: add initial project structure with App component, styles, and SVG assets

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div id="dad">
+        <h1 id="child1">hee</h1>
+        <h2 id="child2">hello</h2>
+      </div>
+      <div id="chahc"></div>
+    </>
+  );
+};
+
+export default App;
